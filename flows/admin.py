f9342096ac133e54c7fdef4585dcc55d06f4c85e
Harshul Jain <harshulj@gmail.com>
2017-02-28 12:32:16 +0530

    Added inline and fields to admin

--- flows/admin.py
+++ flows/admin.py
@@ -2,11 +2,28 @@ from django.contrib import admin
 
 from .models import Flow, Rule
 
+
+class RuleInline(admin.TabularInline):
+    model   = Rule
+    fk_name = 'flow'
+    extra   = 0
+
+
 class FlowAdmin(admin.ModelAdmin):
-    pass
+    inlines         = (RuleInline,)
+    list_display    = ('name', 'type', 'project', 'created_on', 'updated_on',)
+    list_filter     = ('type', 'created_on', 'updated_on',)
+    search_fields   = ('name',)
+    list_display_links = ('name',)
+
 
 class RuleAdmin(admin.ModelAdmin):
-    pass
+    list_display    = ('__unicode__', 'flow', 'next_flow', 'key', 'operation', 'created_on', 'updated_on',)
+    list_filter     = ('operation', 'created_on', 'updated_on',)
+    search_fields   = ('flow', 'next_flow',)
+    #list_display_links = ('name',)
+
+
 
 admin.site.register(Flow, FlowAdmin)
 admin.site.register(Rule, RuleAdmin)
--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
+from flows.models import Flow
+
 from .models import Project
 
+class FlowInline(admin.TabularInline):
+    model   = Flow
+    extra   = 0
+
 class ProjectAdmin(admin.ModelAdmin):
+    inlines         = (FlowInline,)
     list_display    = ('id', 'name', 'state', 'entry', 'created_on', 'updated_on',)
     list_filter     = ('state', 'created_on', 'updated_on',)
     search_fields   = ('name',)
